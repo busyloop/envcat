e1402ca2484892ebc250b6f4e60708604d144e83
moe <moe@busyloop.net>
2023-01-09 12:08:09 +0100

    Preserve trailing newline

--- src/envcat/format/j2.cr
+++ src/envcat/format/j2.cr
@@ -37,14 +37,16 @@ module Envcat
     def write(env)
       Crinja::Undefined.strict = @@strict
 
-      crinja = Crinja.new
+      crinja = Crinja.new(Crinja::Config.new(keep_trailing_newline: true))
 
       crinja.filters["split"] = Crinja.filter({on: nil}) { target.to_s.split(arguments["on"].to_s) }
       crinja.filters["b64encode"] = Crinja.filter { Base64.strict_encode(target.to_s) }
       crinja.filters["b64encode_urlsafe"] = Crinja.filter { Base64.urlsafe_encode(target.to_s) }
       crinja.filters["b64decode"] = Crinja.filter { Base64.decode_string(target.to_s) }
 
-      crinja.from_string(@io_in.gets_to_end).render(@io, env.as(Envcat::Env))
+      buf = IO::Memory.new(16384)
+      IO.copy(@io_in, buf)
+      crinja.from_string(buf.to_s).render(@io, env.as(Envcat::Env))
     end
   end
 end
